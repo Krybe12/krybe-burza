854383fc701a696af3c97841eabbbfacfc663636
Krybe12 <Krybe120@gmail.com>
2021-05-29 13:56:45 +0200

    create price change event

---
+++ database/migrations/2021_05_29_111247_create_change_price_event.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateChangePriceEvent extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      DB::unprepared('
+      CREATE EVENT e_change_prices
+        ON SCHEDULE EVERY 1 HOUR
+        DO 
+        BEGIN
+          CALL p_change_prices();
+        END
+      ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      DB::unprepared('DROP EVENT `e_change_prices`');
+    }
+}
